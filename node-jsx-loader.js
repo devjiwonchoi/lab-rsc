bb73aa746c54d526de46955c7ca4be9082a2bcaf
devjiwonchoi <devjiwonchoi@gmail.com>
2025-08-11 22:25:07 +0200

    use esbuild for jsx

--- node-jsx-loader.js
+++ node-jsx-loader.js
@@ -1,26 +1,26 @@
-import babel from '@babel/core'
-
-const babelOptions = {
-  babelrc: false,
-  ignore: [/\/(build|node_modules)\//],
-  plugins: [['@babel/plugin-transform-react-jsx', { runtime: 'automatic' }]],
-}
+import { transform } from 'esbuild'
 
 export async function load(url, context, defaultLoad) {
   const result = await defaultLoad(url, context, defaultLoad)
   if (result.format === 'module') {
-    const opt = Object.assign({ filename: url }, babelOptions)
-    const newResult = await babel.transformAsync(result.source, opt)
-    if (!newResult) {
-      if (typeof result.source === 'string') {
-        return result
-      }
-      return {
-        source: Buffer.from(result.source).toString('utf8'),
-        format: 'module',
-      }
+    // Transform JSX with esbuild
+    try {
+      const transformed = await transform(
+        typeof result.source === 'string' 
+          ? result.source 
+          : Buffer.from(result.source).toString('utf8'),
+        {
+          loader: 'jsx',
+          jsx: 'automatic',
+          format: 'esm',
+          target: 'node18'
+        }
+      )
+      return { source: transformed.code, format: 'module' }
+    } catch (error) {
+      // If transformation fails, return original
+      return result
     }
-    return { source: newResult.code, format: 'module' }
   }
   return defaultLoad(url, context, defaultLoad)
 }
