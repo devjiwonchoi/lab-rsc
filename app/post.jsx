aaeacd2a1ca08a4cb97bdb947ec68dcb28840408
devjiwonchoi <devjiwonchoi@gmail.com>
2025-08-12 22:02:17 +0200

    comment section with Server Function

--- app/post.jsx
+++ app/post.jsx
@@ -8,8 +8,10 @@ function throwNotFound(cause) {
 
 export default async function BlogPostPage({ postSlug }) {
   let content
+  let comments = []
   try {
     content = await readFile('./app/posts/' + postSlug + '.txt', 'utf8')
+    comments = JSON.parse(await readFile('./app/comments.json', 'utf8'))
   } catch (err) {
     // TODO: render 404 page
     // throwNotFound(err)
@@ -20,6 +22,19 @@ export default async function BlogPostPage({ postSlug }) {
         <a href={'/' + postSlug}>{postSlug}</a>
       </h2>
       <article>{content}</article>
+      <section>
+        <h3>Comments</h3>
+        <form method="POST" action="/_sa/comments">
+          <textarea name="comment" placeholder="Write a comment..." required />
+          <button>Submit</button>
+        </form>
+
+        <ul>
+          {comments.map((comment, index) => (
+            <li key={index}>{comment}</li>
+          ))}
+        </ul>
+      </section>
     </section>
   )
 }
